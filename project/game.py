45bd869a60be9c391a93311c32dadf592dadca7d
Josee uwayezu <rudahuga@gmail.com>
2020-04-17 18:43:10 -0400

    created a program that read user input and validated

---
+++ project/game.py
@@ -0,0 +1,18 @@
+
+if __name__ == '__main__':
+    # print("hello")
+    print("enter a number from 1 and 3:\n")
+    player_input = input()
+    if player_input == "1":
+        print("you are a rock")
+    elif player_input == "2":
+        print("you are a scissor")
+    elif player_input == "3":
+        print("you are a paper")
+    else:
+        print("you entered the wrong option")
+
+
+
+
+
