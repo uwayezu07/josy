f41742e807f94d4c35bfd7a4cd20303b564ae30f
Josee uwayezu <rudahuga@gmail.com>
2020-05-04 23:17:43 -0400

    commit back

--- menuG.py
+++ menuG.py
@@ -1,4 +1,5 @@
-def mainMenu(premenuchoice=()):
+
+def mainMenu():
 
     print("Welcome to Rock, Paper Scissors!\n")
     print("MENU")
@@ -7,47 +8,48 @@ def mainMenu(premenuchoice=()):
     print("(3) Two Player")
     print("(4) quit\n")
 
-    if premenuchoice =="1":
+    premenuchoice = int(input())
+
+    if premenuchoice == 1:
         print(" You have decided to play a one player game against the computer. \n")
         print(" 1) Paper \n")
         print(" 2) Rock \n")
         print(" 3) Scissors \n")
         print(" 4) Quit \n")
-        subMenuChoice = int(input("please choose the weapon from above menu:\n")
-        if subMenuchoice == 1:
+        submenuchoice = int(input("please choose the weapon from above menu:\n"))
+        if submenuchoice == 1:
             print(" You have chosen Paper. ")
-        elif subMenuChoice== 2:
+        elif submenuchoice == 2:
             print(" You have chosen Rock. ")
-        elif subMenuChoice == 3:
+        elif submenuchoice == 3:
             print(" You have chosen Scissors. ")
-        elif subMenuChoice == 4:
+        elif submenuchoice == 4:
             print(" You have chosen to quit. ")
-            return 0
-        else:
-             print(" Invalid menu choice. ")
-    elif(premenuchoice == "2"):
-        print(" You have decided to play a two player game against another human.")
-        print(" 1) Paper \n").
-        print(" 2) Rock \n").
-        print(" 3) Scissors \n").
-        print(" 4) Quit \n").
-        subMenuChoice = (input(" Please choose from the above weapons menu: \n"))
-        if subMenuChoice==1:
-            print(" You have chosen Paper. ")
-        elif subMenuChoice==2:
-            print(" You have chosen Rock. ")
-        elif subMenuChoice==3:
-            print(" You have chosen Scissors.")
-        elif subMenuChoice==4:
-            print(" You have chosen to quit.")
-            return 0
+
         else:
             print(" Invalid menu choice. ")
-    elif(premenuchoice == "" ):
-         print(" Quitting game. Thank you for playing, come again soon. ")
-        return 0
+    elif premenuchoice == 2:
+        print(" You have decided to play a two player game against another human.")
+        print(" 1) Paper \n")
+        print(" 2) Rock \n")
+        print(" 3) Scissors \n")
+        print(" 4) Quit \n")
+        submenuchoice = (input(" Please choose from the above weapons menu: \n"))
+        if submenuchoice ==1:
+            print(" You have chosen Paper. ")
+        elif submenuchoice ==2:
+            print(" You have chosen Rock. ")
+        elif submenuchoice ==3:
+            print(" You have chosen Scissors.")
+        elif submenuchoice ==4:
+            print(" You have chosen to quit.")
+
+        else:
+            print(" Invalid menu choice. ")
+    elif premenuchoice == "3":
+        print(" Quitting game. Thank you for playing, come again soon. ")
     else:
         print(" Invalid menu choice. ")
 
-
-mainMenu()
+if __name__== '__main__':
+   mainMenu()
